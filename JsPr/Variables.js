68b061b4de5fb1565fd74917b48b871cab3e93b4
Kshitij0253 <53930695+Kshitij0253@users.noreply.github.com>
2024-08-17 16:40:33 +0000

    variable done

---
+++ JsPr/Variables.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "ks904125@google.com"
+var accountPassword = "test123"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 const value not be chane or not allowed
+
+accountEmail ="rs.com"
+accountPassword="fsdfsdfsdf"
+accountCity="Bangalore"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+// const means no change lock the value
+
+// let meanss no scope problem 
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/

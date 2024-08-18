db09fe6be34b62b24e427686c968c626547beb98
Kshitij0253 <53930695+Kshitij0253@users.noreply.github.com>
2024-08-18 06:16:59 +0000

    datatype refres

---
+++ JsPr/d_Datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //this words treat all JS code as newer version
+
+// alert("hello"); we are using nodejs,not browser
+
+
+let name = "kshitij"
+let age = 22
+let isLoggedIn = false
+
+
+//number => 2^53
+//bigint
+//string=> '' " "
+//boolean 
+//null (standalone value)
+//undefined = >
+//symbol (for find uniqueness)
+
+
+//NOn - primitives
+//object
+
+console.log(typeof"kshitij");
+console.log(typeof null); //object type
+console.log(typeof undefined);//undeifned type

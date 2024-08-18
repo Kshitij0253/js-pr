a8e0a305be556972bf2fabf9030b90a9af743f63
Kshitij0253 <53930695+Kshitij0253@users.noreply.github.com>
2024-08-18 06:45:03 +0000

    operation conversion

--- JsPr/c_conversion.js
+++ JsPr/c_conversion.js
@@ -1,15 +1,15 @@
 let score = "33sdsd"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 
 let valueInnumber = Number(score)
 
-console.log(typeof valueInnumber);
+// console.log(typeof valueInnumber);
 
-console.log(valueInnumber);
+// console.log(valueInnumber);
 
 
 //basic conversion
@@ -21,12 +21,47 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ***********************operrations ******************
+
+let value = 3
+let negValue  = -value
+
+// console.log(negValue)
+console.log(2+2);
+
+
+let str1 = "hello"
+
+let str2 = " kshitij"
+
+console.log(str1 + str2);
+
+let v1 = "1"+2;
+console.log(v1);
+console.log(typeof v1);
+
+let v2 = 1+"2";
+console.log(v2);
+console.log(typeof v2);
+
+
+let v3  = "1"+ 2 +2;
+console.log(v3);
+console.log(typeof v3);
+
+let v4 = 1 + 2 + "2";
+console.log(v4);
+console.log(typeof v4);
+

e54d6eca4f814dae369d24c7dfb5940c3578e161
Kshitij0253 <53930695+Kshitij0253@users.noreply.github.com>
2024-08-18 06:27:34 +0000

    conversion

---
+++ JsPr/c_conversion.js
@@ -0,0 +1,32 @@
+let score = "33sdsd"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInnumber = Number(score)
+
+console.log(typeof valueInnumber);
+
+console.log(valueInnumber);
+
+
+//basic conversion
+// "33" => 33
+// "33sd" => NaN
+// true => 1;false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);

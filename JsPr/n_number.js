075007a8190a7eaa35e647eb85c7f997de133ede
Kshitij0253 <53930695+Kshitij0253@users.noreply.github.com>
2024-08-19 06:26:54 +0000

    math

---
+++ JsPr/n_number.js
@@ -0,0 +1,48 @@
+//tofixed()
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+
+const otherNumber = 123.8966
+
+// toPrecision use safely
+
+console.log(otherNumber.toPrecision(4));
+
+const hundered = 1000000
+console.log(hundered.toLocaleString('en-IN'));
+
+
+
+//+++++++++++++++++Maths++++++++++++++++++++++
+
+console.log("mathes");
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.min(4,5,6,345,6,7));
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+
+const min = 10;
+
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1)));
+
+
+
+
+
+
+
